7f5935dde64c33a2e4a8ec08bbe04ed4a09f138e
Sihoo Kim <skim22@washcoll.edu>
2025-10-28 12:38:18 -0400

    Create the file named chestertown_bike and initialized the main.dart, index.html, and pubspec.yaml to build initial structure of main page

---
+++ chestertown_bike/android/app/src/main/kotlin/com/example/chestertown_bike/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chestertown_bike
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ chestertown_bike/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Chestertown Bike Map', home: MapScreen());
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? _controller;
+
+  static const LatLng _chestertown = LatLng(39.2090, -76.0660);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chestertown Bike Routes'),
+        backgroundColor: Colors.indigo,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: _chestertown,
+          zoom: 14,
+        ),
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+      ),
+    );
+  }
+}
